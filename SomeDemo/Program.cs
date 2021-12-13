9ddc94809c55a4a2fd5c34c2bb76e3c63095a12a
alexis <alexisd.horton@gmail.com>
2021-12-13 09:26:03 -0500

    Initial commit

---
+++ SomeDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SomeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
